60b027e23d8e8bb02253985f36ebe266933acbab
yanghui <yanghuinobody@qq.com>
2013-11-21 23:55:05 +0800

    alter page class

--- Library/Pager.php
+++ Library/Pager.php
@@ -38,13 +38,18 @@ class Pager
 	 */
 	static $nextPage ;
 
+	/*
+		第一页
+	 */
 	static $firstPage;
 
+	/*
+		最后一页
+	 */
 	static $lastPage;
 	/*
 		分页风格
 	 */
-	
 	static $style ;
 
 	/*
@@ -52,13 +57,31 @@ class Pager
 	 */
 	static $p = 'p';
 
+	/*
+		页码替换标识
+	 */
 	static $B = '_P_';
-
+	
+	/*
+		分页地址
+	 */
 	static $pageUrl ;
 
+	/*
+		不跳转的默认值
+	 */
+	static $unValidHref = 'javascript:;';
+
+	/**
+	 * 构造
+	 * @param [type] $page      [description]
+	 * @param [type] $itemTotal [description]
+	 * @param [type] $pageSize  [description]
+	 * @param [type] $style     [description]
+	 */
 	public function __construct($page, $itemTotal, $pageSize, $style)
 	{
-		self::$page = $page ;
+		self::$page = $page ? $page : 1;
 		self::$pageSize = !empty($pageSize) ? $pageSize : self::$pageSize;
 		self::$itemTotal = $itemTotal;
 		self::$pageCount = intval(ceil($itemTotal/self::$pageSize));
@@ -67,6 +90,10 @@ class Pager
 
 	}
 
+	/**
+	 * 初始化
+	 * @return [type] [description]
+	 */
 	public static function _init()
 	{
 		self::PageUrl();
@@ -76,76 +103,96 @@ class Pager
 		self::setLastUrl();
 	}
 
+	/**
+	 * 分页地址
+	 */
 	public static function  PageUrl()
 	{	
 		parse_str($_SERVER['QUERY_STRING'],$arr);
-		var_dump($arr);
 		$str = '';
 		if (!empty($arr)) {
 			unset($arr[self::$p]);
 			$str = http_build_query($arr);
 		} 
-
 		if (!empty($str)){
 			$str .= '&';
 		}
 		$str .= self::$p . '=' . self::$B; 
-		self::$pageUrl =  $_SERVER['SCRIPT_NAME']  . '?' . $str; 
-		var_dump(self::$pageUrl);
+		self::$pageUrl = 'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']  . '?' . $str; 
 
 	}
 
+	/**
+	 * 分页面地址替换
+	 * @param  [type] $page [description]
+	 * @return [type]       [description]
+	 */
 	public static function pageUrlReplace($page)
 	{
 		return str_replace(self::$B, $page, self::$pageUrl);
 	}
 
+	/**
+	 * 设置前一页
+	 */
 	public static function setPrevUrl()
 	{	
 		$prevPageNums = self::$page - 1;
 		//第一页的前一页为空，或者是第一页
-		if ( $prevPageNums < 0 ) {
-			self::$prevPage = null;
+		if ( $prevPageNums <= 0 ) {
+			self::$prevPage = self::$unValidHref;
+		} else {
+			self::$prevPage = self::pageUrlReplace($prevPageNums);
 		}
-		self::$prevPage = self::pageUrlReplace($prevPageNums);
+		
 	}
 
+	/**
+	 * 设置下一页
+	 */
 	public static function setNextUrl()
 	{	
 		$nextPageNums = self::$page + 1;
 		//最后一页的下一页为空，或者是最后一页
 		if ( $nextPageNums > self::$pageCount ) {
-			self::$prevPage = null;
+			self::$nextPage = self::$unValidHref;
+		} else {
+			self::$nextPage = self::pageUrlReplace($nextPageNums);
 		}
-		self::$nextPage = self::pageUrlReplace($nextPageNums);
+		
 	}
 
+	/**
+	 * 设置第一页
+	 */
 	public static function setFirstUrl()
 	{
 		self::$firstPage = self::pageUrlReplace(1);
 	}
 
+	/**
+	 * 设置最后一页
+	 */
 	public static function setLastUrl()
 	{
 		self::$lastPage = self::pageUrlReplace(self::$pageCount);
 	}
 
+	/**
+	 * 输出分页
+	 * @return [type] [description]
+	 */
 	public static function show()
 	{	
 		$output = '';
 		$style = self::$style;
-
 		switch ($style) {
 			case '1':
-				$output = '<div><span>总共' . self::$itemTotal . '条</span></div>';
+				$before = max(self::$page - 2 , 1);
+				$behind = min(self::$page + 2 , self::$pageCount);
 
-				$before = max(self::$page - 5 , 1);
-				$behind = min(self::$page + 5 , self::$pageCount);
-				if (self::$prevPage) {
-					$output .= '<label><a href="' . self::$prevPage . '"><<</a></label>';
-				} else {
-					$output .= '<label><a href="javascript:;"><<</a></label>';
-				}
+				$output = '<div class="page"><span>总共' . self::$itemTotal . '条</span>';
+				$output .= '<label><a href="' . self::$prevPage . '"><<</a></label>';
 				
 				if ($before != 1) {
 					$output .= '<label><a href="' . self::$firstPage . '">1</a></label>';
@@ -167,15 +214,29 @@ class Pager
 					$output .= '<label>...</label>';
 					$output .= '<label><a href="' . self::$lastPage . '">'.self::$pageCount.'</a></label>';
 				}
-				if (self::$nextPage) {
-					$output .= '<label><a href="' . self::$nextPage . '">>></a></label>';
-				} else {
-					$output .= '<label><a href="javascript:;">>></a></label>';
-				}
+				
+				$output .= '<label><a href="' . self::$nextPage . '">>></a></label>';
+				$output .= '</div>';
 				break;
-			
+			case '2':
+				$output  = '<div class="page"><span>总共' . self::$itemTotal . '条</span>';
+				$output .= '<span>' .self::$page . '/' . self::$pageCount . '</span>';
+				$output .= '<span><a href="' . self::$prevPage . '">上一页</a></span>';
+				$output .= '<span><a href="' . self::$nextPage . '">下一页</a></span>';
+				$output .= '<span><input type="text" name="p" value="' .self::$page. '" size ="3" id="to-page"/></span>';
+				$output .= '<span><input type="button" name="s" value="GO" onclick="' . self::goToJs() . '"/></span>';
+				$output .= '</div>';
+				break;
 		}
 		return $output;
+	}
+
+	/**
+	 * 跳转js
+	 * @return [type] [description]
+	 */
+	public static function goToJs(){
+		return "window.location.href='". self::$pageUrl . "'.replace('".self::$B."',document.getElementById('to-page').value)";
 	}
 }	
 
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
 
 include_once './Library/Pager.php';
-$page = new Pager(1,100,10,1);
+$p = isset($_GET['p']) ? $_GET['p'] : 1;
+$page = new Pager($p,100,5,1);
 
-$d= Pager::show();
+
 ?>
 <!doctype html> 
 <html> 
@@ -12,7 +13,19 @@ $d= Pager::show();
     <title></title> 
 </head>
 <body>
+	<style type="text/css">
+		.page{padding: 5px;border: 1px dashed  #000;}
+		.page label{margin: 3px;width:20px;display: inline-block;}
+		a {text-decoration: none;}
+	</style>
     <h1></h1>
     <?php echo Pager::show();?>
+
+    <br/>
+    <?php 
+    $page2 = new Pager($p,100,5,2);
+    echo Pager::show();
+    ?>
+
 </body>
 </html>
